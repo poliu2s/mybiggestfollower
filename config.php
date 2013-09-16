5ce8f1373b4d16c20b223decb7f287b4e00c1953
Po Liu <po.liu@alumni.ubc.ca>
2013-09-15 23:39:05 -0700

    Update config.php

--- config.php
+++ config.php
@@ -6,5 +6,5 @@
  */
 
 define('CONSUMER_KEY', 'n2LcXL5hP57qigTSPl1ZBA');
-define('CONSUMER_SECRET', 'BNVqbkxb9JWQ0CzG6dgVD3C9RM805de6TJhj3GnI');
+define('CONSUMER_SECRET', '**************************************************');
 define('OAUTH_CALLBACK', 'http://mybiggestfollower-pomart.rhcloud.com/callback.php');
